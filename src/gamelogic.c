13a4e6f52cfef353f632d7da605212b041d976ae
Lemoncreme <schwalegos@gmail.com>
2018-10-11 20:39:25 -0600

    broke collisions again

--- src/gamelogic.c
+++ src/gamelogic.c
@@ -29,10 +29,10 @@ void game_update(int input[BUTTON_COUNT]) {
 	int tile_y = floor((player.position_y + player.velocity_y + 16) / TILE_HEIGHT);
 	int feet_y = floor((player.position_y + player.velocity_y + 33) / TILE_HEIGHT);
 	int top_y = floor((player.position_y + player.velocity_y - 1) / TILE_HEIGHT);
-	int right_x = floor((player.position_x + player.velocity_x + 33) / TILE_WIDTH);
-	int left_x = floor((player.position_x + player.velocity_x - 1) / TILE_WIDTH);
-	int tile_xl = floor((player.position_x + player.velocity_x) / TILE_WIDTH);
-	int tile_xr = floor((player.position_x + player.velocity_x + 32) / TILE_WIDTH);
+	int right_x = floor((player.position_x + player.velocity_x + PLAYER_RIGHT + 1) / TILE_WIDTH);
+	int left_x = floor((player.position_x + player.velocity_x - PLAYER_LEFT - 1) / TILE_WIDTH);
+	int tile_xl = floor((player.position_x + player.velocity_x - PLAYER_LEFT) / TILE_WIDTH);
+	int tile_xr = floor((player.position_x + player.velocity_x + PLAYER_RIGHT) / TILE_WIDTH);
 
 	player.tile_x = tile_x;
 	player.tile_y = tile_y;
@@ -62,14 +62,14 @@ void game_update(int input[BUTTON_COUNT]) {
 	if (tile_at(right_x, tile_y) || right_x >= LEVEL_WIDTH) {
 		if (player.velocity_x > 0)
 			player.velocity_x = 0;
-		player.position_x = (right_x - 1) * TILE_WIDTH;
+		player.position_x = (right_x - 1) * TILE_WIDTH + PLAYER_MARGIN;
 	}
 
 	//Left collision
 	if (tile_at(left_x, tile_y) || left_x < 0) {
 		if (player.velocity_x < 0)
 			player.velocity_x = 0;
-		player.position_x = (left_x + 1) * TILE_WIDTH;
+		player.position_x = (left_x + 1) * TILE_WIDTH - PLAYER_MARGIN;
 	}
 
 	//Apply player velocity
