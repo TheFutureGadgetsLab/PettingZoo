a51519f44352ddf389b02b2b6785925454cfee14
haydn-jones <haydnjonest@gmail.com>
2018-11-05 15:37:20 -0700

    wat

--- src/neural_network.cpp
+++ src/neural_network.cpp
@@ -4,14 +4,16 @@
 #include <stdint.h>
 
 uint8_t * generate_chromosome(uint8_t in_h, uint8_t in_w, uint8_t hlc, uint16_t npl);
+void print_chromosome(uint8_t *chrom);
 
 int main()
 {
     uint8_t *chrom = NULL;
-    printf("Running NN with %d by %d inputs, %d outputs, %d hidden layers, and %d nodes per layer.\n\n", 
+    printf("Running NN with %d by %d inputs, %d outputs, %d hidden layers, and %d nodes per layer.\n", 
         IN_H, IN_W, OUTPUT_SIZE, HLC, NPL);
 
     chrom = generate_chromosome(IN_H, IN_W, HLC, NPL);
+    print_chromosome(chrom);
 
     free(chrom);
     return 0;
@@ -25,6 +27,99 @@ uint8_t * generate_chromosome(uint8_t in_h, uint8_t in_w, uint8_t hlc, uint16_t 
     float *tmp;
     int r, c, hl, base;
     
+    // Adj matrices multiplied by 4 as they are floats
+    in_act = in_h * in_w;
+    in_adj = (in_h * in_w) * npl * 4;
+    hidden_act = hlc * npl;
+    hidden_adj = (hlc - 1) * (npl * npl) * 4;
+    out_adj = (npl * OUTPUT_SIZE) * 4;
+
+    total_size = 5 + in_act + in_adj + hidden_act + hidden_adj + out_adj;
+    
+    chrom = (uint8_t *)malloc(total_size);
+
+    chrom[0] = in_w;
+    chrom[1] = in_h;
+    *(((uint16_t *)chrom) + 1) = npl;
+    chrom[4] = hlc;
+    
+    base = 5;
+    // Generate input act matrix
+    for (r = 0; r < in_h; r++) {
+        for (c = 0; c < in_w; c++) {
+            chrom[base] = random() % 2;
+            printf("%d\t", chrom[base]);
+            base++;
+        }
+        puts("");
+    }
+
+    // Generate input adj matrix
+    tmp = (float *)(chrom + base);
+    for (r = 0; r < in_h * in_w; r++) {
+        for (c = 0; c < npl; c++) {
+            *tmp = (float)random() / (float)random();
+            printf("%0.2lf\t", *tmp);
+            tmp++;
+            base++;
+        }
+        puts("");
+    }
+
+    // Generate hidden act matrix
+    for (r = 0; r < npl; r++) {
+        for (c = 0; c < hlc; c++) {
+            chrom[base] = random() % 2;
+            printf("%d\t", chrom[base]);
+            base++;
+        }
+        puts("");
+    }
+
+    // Generate hidden adj matrices
+    tmp = (float *)(chrom + base);
+    for (hl = 0; hl < hlc - 1; hl++) {
+        for (r = 0; r < npl; r++) {
+            for (c = 0; c < npl; c++) {
+                *tmp = (float)random() / (float)random();
+                printf("%0.2lf\t", *tmp);
+                tmp++;
+                base++;
+            }
+            puts("");
+        }
+        puts("");
+    }
+
+    // Generate out adj matrix
+    tmp = (float *)(chrom + base);
+    for (r = 0; r < npl; r++) {
+        for (c = 0; c < OUTPUT_SIZE; c++) {
+            *tmp = (float)random() / (float)random();
+            printf("%0.2lf\t", *tmp);
+            tmp++;
+            base++;
+        }
+        puts("");
+    }
+
+    return chrom;
+}
+
+void print_chromosome(uint8_t *chrom)
+{
+    printf("-------------------------------------------\n");
+    size_t total_size, in_act, in_adj, hidden_act, hidden_adj, out_adj;
+    uint8_t in_w, in_h, hlc;
+    uint16_t npl;
+    float *tmp;
+    int r, c, hl, base;
+
+    in_w = chrom[0];
+    in_h = chrom[1];
+    npl = *((uint16_t *)chrom + 1);
+    hlc = chrom[4];
+
     in_act = in_h * in_w;
     in_adj = (in_h * in_w) * npl * 4;
     hidden_act = hlc * npl;
@@ -34,89 +129,59 @@ uint8_t * generate_chromosome(uint8_t in_h, uint8_t in_w, uint8_t hlc, uint16_t 
     // Adj matrices multiplied by 4 as they are floats
     total_size = 5 + in_act + in_adj + hidden_act + hidden_adj + out_adj;
 
-    printf("Generating chromosome with the following paramenters:\n");
-    printf("  IN_H:\t%d\n  IN_W:\t%d\n  HLC:\t%d\n  NPL:\t%d\n",
-              in_h, in_w, hlc, npl);
-    printf("\n  Total size: %0.2lfKB\n\n", total_size / 1000.0);
-    
-    chrom = (uint8_t *)malloc(total_size);
-
-    chrom[0] = in_w;
-    chrom[1] = in_h;
-    *((uint16_t *)chrom + 1) = npl;
-    chrom[4] = HLC;
-
-    printf("IN_W: %d\n", chrom[0]);    
-    printf("IN_H: %d\n", chrom[1]);    
-    printf("NPL: %d\n", *((uint16_t *)chrom + 1));    
-    printf("HLC: %d\n", chrom[4]);    
-
     base = 5;
-    printf("Base: %d\n", base);
     // Generate input act matrix
     for (r = 0; r < in_h; r++) {
         for (c = 0; c < in_w; c++) {
-            chrom[base + r * in_w + c] = random() % 2;
-            printf("%d\t", chrom[base + r * in_w + c]);
+            printf("%d\t", chrom[base]);
+            base++;
         }
         printf("\n");
     }
-    base += in_act;
-    
-    printf("Base: %d\n", base);
-    // Generate input adj matrix
-    tmp = (float *)(chrom + base);
-    for (r = 0; r < in_h * in_w; r++) {
-        for (c = 0; c < npl; c++) {
-            *tmp = (float)random() / (float)random();
-            printf("%.2lf\t", *tmp);
-            tmp++;
-        }
-        printf("\n");
-    }
-    base += in_adj;
 
-    printf("Base: %d\n", base);
-    // Generate hidden act matrix
+    tmp = (float *)(chrom + base);
+    for (r = 0; r < in_w * in_h; r++) {
+        for (c = 0; c < npl; c++) {
+            printf("%0.2lf\t", *tmp);
+            tmp++;
+            base++;
+        }
+        puts("");
+    }
+
     for (r = 0; r < npl; r++) {
         for (c = 0; c < hlc; c++) {
-            chrom[base + r * hlc + c] = random() % 2;
-            printf("%d\t", chrom[base + r * hlc + c]);
+            printf("%d\t", chrom[base]);
+            base++;
         }
-        printf("\n");
+        puts("");
     }
-    base += hidden_act;
 
-    // Generate hidden adj matrices
-    printf("Base: %d\n", base);
     tmp = (float *)(chrom + base);
     for (hl = 0; hl < hlc - 1; hl++) {
-        for (r = 0; r < in_h * in_w; r++) {
+        for (r = 0; r < npl; r++) {
             for (c = 0; c < npl; c++) {
-                *tmp = (float)random() / (float)random();
-                printf("%.2lf\t", *tmp);
+                printf("%0.2lf\t", *tmp);
                 tmp++;
+                base++;
             }
             puts("");
         }
         puts("");
     }
-    base += hidden_adj;
-    
-    printf("Base: %d\n", base);
-    // Generate out adj matrix
+
     tmp = (float *)(chrom + base);
     for (r = 0; r < npl; r++) {
         for (c = 0; c < OUTPUT_SIZE; c++) {
-            *tmp = (float)random() / (float)random();
-            printf("%.2lf\t", *tmp);
+            printf("%0.2lf\t", *tmp);
             tmp++;
+            base++;
         }
-        printf("\n");
+        puts("");
     }
-
-    base += out_adj;
-    printf("Base: %d\n", base);
-
-    return chrom;
+    printf("\nChromosome:\n");
+    printf("in_w:\t%d\nin_h:\t%d\nnpl:\t%d\nhlc:\t%d\n",
+              in_h, in_w, npl, hlc);
+    printf("\nTotal size: %0.2lfKB\n\n", total_size / 1000.0);
+    printf("-------------------------------------------\n");
 }
