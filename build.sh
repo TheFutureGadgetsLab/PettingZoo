c4fa40c978908fa1e75406d442f02091fd18585a
Haydn Jones <haydnjonest@gmail.com>
2019-08-31 10:17:49 -0600

    Initial cleaning

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd build;
-make;
-if [ "$?" -eq "0" ]
-    then
-        echo "BUILT!"
-        ./pettingzoo;
-fi
--- scripts/mod_128.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-../build/trainGPU -n 128 -l 1 -c 5000 -g 500 -o 128_1
-../build/trainGPU -n 128 -l 2 -c 5000 -g 500 -o 128_2
-../build/trainGPU -n 128 -l 3 -c 5000 -g 500 -o 128_3
-../build/trainGPU -n 128 -l 4 -c 5000 -g 500 -o 128_4
