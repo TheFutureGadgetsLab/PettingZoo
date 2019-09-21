550c73b003b5825f9cb75ee2c6952f9e4a7464c7
Haydn Jones <haydnjonest@gmail.com>
2019-09-20 23:29:41 -0600

    Added help button (h), restart and regen game

--- main.py
+++ main.py
@@ -3,23 +3,31 @@ from game.rendering.renderer import Renderer
 from time import time
 
 def main():
+    num_chunks = 10
     seed = int(time())
+
     renderer = Renderer()
-    game = Game(num_chunks=50, seed=seed)
+    game = Game(num_chunks=num_chunks, seed=seed)
 
     renderer.new_game_setup(game)
 
     while renderer.running:
-        keys = renderer.get_input()
+        keys, req = renderer.get_input()
+
+        # Check if user is trying to restart / generate new game
+        if req in [renderer.RESTART, renderer.NEW_GAME]:
+            seed = seed if req == renderer.RESTART else int(time())
+            game = Game(num_chunks=num_chunks, seed=seed)
+            renderer.new_game_setup(game)
+            continue
 
         game.update(keys)
 
         if game.game_over:
-            seed = int(time())
-            game = Game(num_chunks=50, seed=seed)
+            game = Game(num_chunks=num_chunks, seed=seed)
             renderer.new_game_setup(game)
             continue
-    
+
         renderer.draw_state(game, keys)
 
 if __name__ == "__main__":
