092125d4442238ac9340f79b553670ae397d581a
haydn-jones <haydnjonest@gmail.com>
2019-10-11 07:24:37 -0400

    Cleaned up layer configuration

--- models/utils/pytorch_layer_config.py
+++ models/utils/pytorch_layer_config.py
@@ -1,4 +1,4 @@
-import torch
+import torch.nn as nn
 
 def config_to_sequential(config, view_size):
     """
@@ -18,13 +18,11 @@ def config_to_sequential(config, view_size):
 
     torch_layers = []
 
-    # Initialize prev_dim (for first layer) to be input window size 
-    
+    # Initialize prev_dim (for first layer) to be input window size
     prev_dim = view_size.x * view_size.y
 
     for layer_config in config:
-        layer_type = layer_config[0]
-        layer_args = layer_config[1:]
+        layer_type, layer_args = layer_config[0], layer_config[1:]
 
         if layer_type == "linear": # Linear layer
             prev_dim, layer = get_linear(layer_args, prev_dim)
@@ -35,28 +33,28 @@ def config_to_sequential(config, view_size):
 
         torch_layers.append(layer)
 
-    _, button_layer = get_linear([3], prev_dim)
-    final_act       = get_act(["tanh"])
+    # Add output layer
+    torch_layers.extend([
+        nn.Linear(prev_dim, 3, bias=False),
+        nn.Tanh(),
+    ])
 
-    torch_layers.append(button_layer)
-    torch_layers.append(final_act)
-    
-    return torch.nn.Sequential(*torch_layers)
+    return nn.Sequential(*torch_layers)
 
 def get_linear(layer_args, prev_dim):
-    layer = torch.nn.Linear(in_features=prev_dim, out_features=layer_args[0], bias=False)
+    layer = nn.Linear(in_features=prev_dim, out_features=layer_args[0], bias=False)
     new_dim = layer_args[0]
 
     return new_dim, layer
 
 def get_act(layer_args):
-    if layer_args[0] == "relu":
-        layer = torch.nn.ReLU()
-    elif layer_args[0] == "tanh":
-        layer = torch.nn.Tanh()
-    elif layer_args[0] == "sigmoid":
-        layer = torch.nn.Sigmoid()
-    else:
-        raise ValueError
+    activations = {
+        "relu"    : nn.ReLU,
+        "sigmoid" : nn.Sigmoid,
+        "tanh"    : nn.Tanh,
+    }
 
-    return layer
+    if layer_args[0] not in activations:
+        raise ValueError("Unknown activation function")
+
+    return activations[layer_args[0]]()
